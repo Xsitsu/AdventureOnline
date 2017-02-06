80d7136739590508f6b6737126cc17c97fe83c4b
Xsitsu <jacoblocke21@live.com>
2017-02-06 13:18:37 -0800

    Created Vector2 type.

---
+++ GameShared/vector2.hpp
@@ -0,0 +1,72 @@
+#ifndef VECTOR2_HPP_INCLUDE
+#define VECTOR2_HPP_INCLUDE
+
+#include <string>
+
+struct Vector2
+{
+    int x;
+    int y;
+
+    Vector2()
+    {
+        this->x = 0;
+        this->y = 0;
+    }
+
+    Vector2(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    Vector2(Vector2& rhs)
+    {
+        this->x = rhs.x;
+        this->y = rhs.y;
+    }
+
+    Vector2& operator=(Vector2& rhs)
+    {
+        this->x = rhs.x;
+        this->y = rhs.y;
+        return *this;
+    }
+
+    bool operator==(Vector2& rhs)
+    {
+        return (this->x == rhs.x && this->y == rhs.y);
+    }
+
+    bool operator!=(Vector2& rhs)
+    {
+        return !(*this == rhs);
+    }
+
+    Vector2 operator+(Vector2& rhs)
+    {
+        return Vector2(this->x + rhs.x, this->y + rhs.y);
+    }
+
+    Vector2 operator-(Vector2& rhs)
+    {
+        return Vector2(this->x - rhs.x, this->y - rhs.y);
+    }
+
+    Vector2 operator*(int num)
+    {
+        return Vector2(this->x * num, this->y * num);
+    }
+
+    Vector2 operator/(int num)
+    {
+        return Vector2(this->x / num, this->y / num);
+    }
+
+    std::string ToString()
+    {
+        return std::string(x) + "/" + y;
+    }
+
+};
+#endif // VECTOR2_HPP_INCLUDE
