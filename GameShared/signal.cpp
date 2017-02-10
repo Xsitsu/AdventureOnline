37d0f57fb083ff3b11be41e616db0b28e3370749
Xsitsu <jacoblocke21@live.com>
2017-02-10 13:12:17 -0800

    Created basic signal class. Still needs testing.

---
+++ GameShared/signal.cpp
@@ -0,0 +1,42 @@
+#include "signal.hpp"
+
+Signal::Signal()
+{
+
+}
+
+void Signal::Fire(SignalArgs* args)
+{
+    callback_iter iter = this->connected_funcs.begin();
+    for (iter; iter != this->connected_funcs.end(); ++iter)
+    {
+        (*iter)(args);
+    }
+}
+
+void Signal::DisconnectFunction(signal_callback callback)
+{
+    callback_iter iter = this->connected_funcs.begin();
+    for (iter; iter != this->connected_funcs.end(); ++iter)
+    {
+        if (*iter == callback)
+        {
+            this->connected_funcs.erase(iter);
+            return;
+        }
+    }
+}
+
+SignalListener::SignalListener(Signal* signal, signal_callback callback) : signal(signal), callback(callback), is_connected(true)
+{
+
+}
+
+void SignalListener::Disconnect()
+{
+    if (this->is_connected)
+    {
+        this->is_connected = false;
+        this->signal->DisconnectFunction(this->callback);
+    }
+}
---
+++ GameShared/signal.hpp
@@ -0,0 +1,47 @@
+#ifndef SIGNAL_HPP_INCLUDE
+#define SIGNAL_HPP_INCLUDE
+
+#include <list>
+
+#include "main.h"
+
+struct DLL_EXPORT SignalArgs
+{
+    virtual ~SignalArgs() {}
+};
+
+typedef DLL_EXPORT void (*signal_callback)(const SignalArgs* args);
+typedef std::list<signal_callback>::iterator callback_iter;
+
+class SignalListener;
+
+class DLL_EXPORT Signal
+{
+friend class SignalListener;
+
+protected:
+    std::list<signal_callback> connected_funcs;
+
+    void DisconnectFunction(signal_callback callback);
+
+public:
+    Signal();
+
+    SignalListener Connect(signal_callback callback);
+    void Fire(SignalArgs* args);
+};
+
+class DLL_EXPORT SignalListener
+{
+protected:
+    bool is_connected;
+    signal_callback callback;
+    Signal* signal;
+
+public:
+    SignalListener(Signal* signal, signal_callback callback);
+    void Disconnect();
+
+};
+
+#endif // SIGNAL_HPP_INCLUDE
